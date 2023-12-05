ce838ed171b12fc7575788b91a119789245f2ced
Jonas Hoerl <jonas.hoerl@andrena.de>
2023-12-05 12:24:49 +0100

    Day3.1: chatGPT refactor

--- src/Day3/part1.js
+++ src/Day3/part1.js
@@ -1,5 +1,62 @@
 import {readFile} from "../common/importer.js";
 
+
+const numbers = (line, y) => {
+    const digitRegex = /[1234567890]/;
+
+    return line.split('')
+        .map((character, x) => [x, y, character])
+        .filter(([x, y, character]) => digitRegex.test(character))
+        .reduce(positionToNumber(), [])
+}
+
+const positionToNumber = () => {
+    return (posNums, [x, y, number]) => {
+        if (posNums.length === 0) {
+            return posNums.concat([[x, y, number]])
+        }
+
+        const previousX = posNums[posNums.length - 1][0];
+        const previousY = posNums[posNums.length - 1][1];
+        const previousNumber = posNums[posNums.length - 1][2];
+
+        if (previousX === x - 1 && previousY === y) {
+            let composedNumber = previousNumber + number;
+            posNums[posNums.length - 1][2] = composedNumber;
+
+            if (posNums.length === 1) {
+                return posNums.concat([[x, y, composedNumber]])
+            }
+
+            const prepreviousX = posNums[posNums.length - 2][0];
+            const prepreviousY = posNums[posNums.length - 2][1];
+            const prepreviousNumber = posNums[posNums.length - 2][2];
+
+            if (prepreviousX === x - 2 && prepreviousY === y) {
+                composedNumber = prepreviousNumber + number;
+                posNums[posNums.length - 1][2] = composedNumber;
+                posNums[posNums.length - 2][2] = composedNumber;
+            }
+
+            return posNums.concat([[x, y, composedNumber]])
+        } else {
+            return posNums.concat([[x, y, number]])
+        }
+    };
+}
+
+const groupNumbers = () => (result, number) => {
+    if (result.length > 0 && result[result.length - 1][0][2] === number[2]) {
+        const lastArray = result[result.length - 1];
+        return [...result.slice(0, -1), [...lastArray, number]];
+    }
+    return result.concat([[number]]);
+};
+
+const numberHasValidPosition = () => {
+    return numbers => numbers.some(([x, y, number]) => validPositions.some(([validX, validY]) => x === validX && y === validY))
+}
+
 const symbolPositions = (line, y) => {
     const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]+/
 
@@ -22,69 +79,11 @@ const positionsAroundSymbol = (x, y) => {
     ]
 }
 
-function positionToNumber() {
-    return (posNums, [x, y, number]) => {
-        if (posNums.length == 0) {
-            return posNums.concat([[x, y, number]])
-        }
-
-        const previousX = posNums[posNums.length - 1][0];
-        const previousY = posNums[posNums.length - 1][1];
-        const previousNumber = posNums[posNums.length - 1][2];
-
-        if (previousX == x - 1 && previousY == y) {
-            let composedNumber = previousNumber + number;
-            posNums[posNums.length - 1][2] = composedNumber;
-
-            if (posNums.length == 1) {
-                return posNums.concat([[x, y, composedNumber]])
-            }
-
-            const prepreviousX = posNums[posNums.length - 2][0];
-            const prepreviousY = posNums[posNums.length - 2][1];
-            const prepreviousNumber = posNums[posNums.length - 2][2];
-
-            if (prepreviousX == x - 2 && prepreviousY == y) {
-                composedNumber = prepreviousNumber + number;
-                posNums[posNums.length - 1][2] = composedNumber;
-                posNums[posNums.length - 2][2] = composedNumber;
-            }
-
-            return posNums.concat([[x, y, composedNumber]])
-        } else {
-            return posNums.concat([[x, y, number]])
-        }
-    };
-}
-
-const numbers = (line, y) => {
-    const digitRegex = /[1234567890]/;
-    const numbers = line.split('')
-        .map((character, x) => [x, y, character])
-        .filter(([x, y, character]) => digitRegex.test(character))
-        .reduce(positionToNumber(), [])
-    return numbers;
-}
-
 const validPositions = readFile()
     .map((line, index) => symbolPositions(line, index))
     .reduce((a, b) => a.concat(b), [])
     .map(([x, y]) => positionsAroundSymbol(x, y))
     .reduce((a, b) => a.concat(b), [])
-
-function groupNumbers() {
-    return (result, number) => {
-        if (result.length > 0 && result[result.length - 1][0][2] === number[2]) {
-            const lastArray = result[result.length - 1];
-            return [...result.slice(0, -1), [...lastArray, number]];
-        }
-        return result.concat([[number]]);
-    }
-}
-
-function numberHasValidPosition() {
-    return numbers => numbers.some(([x, y, number]) => validPositions.some(([validX, validY]) => x == validX && y == validY))
-}
 
 const sumOfValidNumbers = readFile()
     .map((line, index) => numbers(line, index))
