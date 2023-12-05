4512bef0e20b617ec63bb883aa8a85ce3dd01568
Jonas Hoerl <jonas.hoerl@andrena.de>
2023-12-05 17:03:01 +0100

    Day4.2: first solution

--- src/Day4/part2.js
+++ src/Day4/part2.js
@@ -1,4 +1,4 @@
-import {readExample} from "../common/importer.js";
+import {readFile} from "../common/importer.js";
 import {numbers, winningNumbers, yourNumbers} from "./part1.js";
 
 export const cardNumber = (line) => {
@@ -11,16 +11,13 @@ function amountOfExtraCards(yourNumbers, winningNumbers) {
     return yourNumbers.filter(number => winningNumbers.includes(number)).length;
 }
 
-const cardNumbersAndExtraCards = readExample()
+const cardNumbersAndExtraCards = readFile()
     .map(line => [cardNumber(line), winningNumbers(line), yourNumbers(line)])
     .map(([cardNumber, winningNumbers, yourNumbers]) => [cardNumber[0], amountOfExtraCards(yourNumbers, winningNumbers)])
 
 const copyCard = (cardNumber, total) => {
-    console.log('cardNumber: ' + cardNumber);
-    const extraCards = cardNumbersAndExtraCards[cardNumber-1][1];
-    console.log('extraCards for ' + cardNumber + ' : ' + extraCards);
+    const extraCards = cardNumbersAndExtraCards[cardNumber - 1][1];
     let newTotal = total + 1;
-    console.log(newTotal);
 
     for (let i = cardNumber + 1; i < cardNumber + extraCards + 1; i++) {
         newTotal = copyCard(i, newTotal);
@@ -28,11 +25,8 @@ const copyCard = (cardNumber, total) => {
     return newTotal;
 }
 
-console.log(cardNumbersAndExtraCards);
-
 const originalAndCopies = cardNumbersAndExtraCards
-    .map(([cardNumber, extraCards]) => copyCard(cardNumber, 0))
+    .map(([cardNumber, extraCards]) => copyCard(cardNumber, 0)    )
     .reduce((a, b) => a + b, 0);
-
 
 console.log(originalAndCopies)
