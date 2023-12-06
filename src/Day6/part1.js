7853e45833d4c65877ea14b1586814d6e0edf131
jonas.hoerl <jonas_hoerl@web.de>
2023-12-06 21:43:12 +0100

    Day6.1: solution

--- src/Day6/part1.js
+++ src/Day6/part1.js
@@ -1,10 +1,29 @@
-import {readExample, readFile} from "../common/importer.js";
+import {readFile} from "../common/importer.js";
 
-const file = readExample();
+const file = readFile();
 const times = file[0].slice(10).trim().match(/(\d+)/g).map(Number);
 const distances = file[1].slice(10).trim().match(/(\d+)/g).map(Number);
 
+const waysToBeat = (index) => {
+    let wins = 0;
+    const timeAvailable = times[index];
+    const distanceToBeat = distances[index];
 
+    for (let timeCharging = 0; timeCharging <= timeAvailable; timeCharging++){
+        const timeMoving = timeAvailable - timeCharging;    // [ms]
+        const velocity = timeCharging                       // [m/s]
+        const distanceMoved = timeMoving * velocity;        // [mm]
 
+        if(distanceMoved > distanceToBeat){
+            wins++;
+        }
+    }
 
-console.log(distances)
+    return wins;
+}
+
+const result = Array.from(times.keys())
+    .map(index => waysToBeat(index))
+    .reduce((a,b) => a * b, 1)
+
+console.log(result);
