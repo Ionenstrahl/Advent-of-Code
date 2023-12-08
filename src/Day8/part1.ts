ab0bcb1880d222a25800bfcbc2b4504a80218a8f
jonas.hoerl <jonas_hoerl@web.de>
2023-12-08 08:30:35 +0100

    Day8.1: working example

--- src/Day8/part1.ts
+++ src/Day8/part1.ts
@@ -1,8 +1,27 @@
 import {readExample, readFile} from "../common/importer.js";
-import {parseInstructions, parseNodes} from "./parser";
+import {node, parseInstructions, parseNodes} from "./parser";
 
 const file: string[] = readExample();
 const instructions: string = parseInstructions(file);
-const nodes = parseNodes(file);
+const nodes: node[] = parseNodes(file);
 
-console.log(nodes);
+let currentNode: node = nodes[0];
+let steps = 0;
+
+function getNextNodeName(instruction: string) {
+    if (instruction === 'R') {
+        return currentNode.right;
+    }
+    return currentNode.left;
+}
+
+while(currentNode.name !== 'ZZZ'){
+    const instruction: string = instructions[steps % (instructions.length -1 )]
+
+    let nextNodeName = getNextNodeName(instruction);
+
+    currentNode = nodes.find(node => node.name === nextNodeName);
+    steps++;
+}
+
+console.log(steps);
