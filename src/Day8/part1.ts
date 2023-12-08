b83fc47eaee3c0e450c3e3277ef38b721630f82d
jonas.hoerl <jonas_hoerl@web.de>
2023-12-08 09:15:38 +0100

    Day8.1: first solution

--- src/Day8/part1.ts
+++ src/Day8/part1.ts
@@ -1,27 +1,34 @@
-import {readExample, readFile} from "../common/importer.js";
+import {readFile} from "../common/importer.js";
 import {node, parseInstructions, parseNodes} from "./parser";
 
-const file: string[] = readExample();
+const file: string[] = readFile();
 const instructions: string = parseInstructions(file);
 const nodes: node[] = parseNodes(file);
 
-let currentNode: node = nodes[0];
-let steps = 0;
 
-function getNextNodeName(instruction: string) {
+let currentNode: node = nodes.find(node => node.name === 'AAA');
+let steps: number = 0;
+
+while(currentNode.name !== 'ZZZ'){
+    const instruction: string = getInfiniteInstructions(steps);
+
+    let nextNodeName: string = getNextNodeName(instruction);
+
+    currentNode = nodes.find(node => node.name === nextNodeName);
+
+    steps++;
+}
+
+function getInfiniteInstructions(stepsTaken: number): string {
+    return instructions[stepsTaken % (instructions.length - 1) ];
+}
+
+function getNextNodeName(instruction: string): string {
+
     if (instruction === 'R') {
         return currentNode.right;
     }
     return currentNode.left;
-}
-
-while(currentNode.name !== 'ZZZ'){
-    const instruction: string = instructions[steps % (instructions.length -1 )]
-
-    let nextNodeName = getNextNodeName(instruction);
-
-    currentNode = nodes.find(node => node.name === nextNodeName);
-    steps++;
 }
 
 console.log(steps);
