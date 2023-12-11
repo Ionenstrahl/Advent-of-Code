dcf3f8affa5257e39cbd7311b1354558ceabe064
jonas.hoerl <jonas_hoerl@web.de>
2023-12-11 17:31:33 +0100

    Day11.2: not working state

--- src/Day11/part1.ts
+++ src/Day11/part1.ts
@@ -1,7 +1,8 @@
 import {readExample, readFile} from "../common/importer";
 import {Galaxy} from "./galaxy";
 
-const input = readFile();
+const input = readExample();
+
 
 const galaxies: Galaxy[] = input
     .flatMap((line, y) => line.split('').map((skyTile, x) => ({
@@ -22,27 +23,32 @@ const emptyRows: number[] = input
     .filter(({line}) => !line.includes('#'))
     .map(({index}) => index)
 
-const correctedGalaxies = galaxies
-    .map(galaxy => {
-        galaxy.x += xShift(galaxy);
-        galaxy.y += yShift(galaxy);
-        return galaxy
-    })
+let correctedGalaxies;
+setCorrectedGalaxies(1000000);
 
-function xShift(galaxy: Galaxy) {
+export function setCorrectedGalaxies(expansion) {
+    correctedGalaxies = galaxies
+        .map(galaxy => {
+            galaxy.x += xShift(galaxy, expansion);
+            galaxy.y += yShift(galaxy, expansion);
+            return galaxy
+        })
+}
 
-    return emptyColumns.filter(col => col < galaxy.x).length;
+function xShift(galaxy: Galaxy, expansion: number) {
+    return emptyColumns.filter(col => col < galaxy.x).length * (expansion - 1);
+}
+
+function yShift(galaxy: Galaxy, expansion: number) {
+    return emptyRows.filter(row => row < galaxy.y).length * (expansion - 1);
 
 }
 
-function yShift(galaxy: Galaxy) {
-    return emptyRows.filter(row => row < galaxy.y).length;
-
+export function distanceSum() {
+    return correctedGalaxies
+        .flatMap(g1 => relevantCompareGalaxies(g1).map(g2 => calculateDistance(g1, g2)))
+        .reduce((sum, distance) => sum + distance, 0);
 }
-
-const distanceSum = correctedGalaxies
-    .flatMap(g1 => relevantCompareGalaxies(g1).map(g2 => calculateDistance(g1, g2)))
-    .reduce((sum, distance) => sum + distance, 0);
 
 function relevantCompareGalaxies(g1: Galaxy): Galaxy[] {
     return correctedGalaxies
@@ -54,4 +60,4 @@ function calculateDistance(g1: Galaxy, g2: Galaxy): number {
     return Math.abs(g1.x - g2.x) + Math.abs(g1.y - g2.y)
 }
 
-console.log(distanceSum)
+console.log(distanceSum())
---
+++ src/Day11/part2.ts
@@ -0,0 +1,6 @@
+import {distanceSum, setCorrectedGalaxies} from "./part1";
+
+const expansion = 100;
+setCorrectedGalaxies(expansion);
+
+console.log(distanceSum())
