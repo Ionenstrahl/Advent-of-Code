969c3c389a1ea291de4cf3e54bd44de3f1d29680
jonas.hoerl <jonas_hoerl@web.de>
2023-12-11 10:57:07 +0100

    Day10.1: working example

--- src/Day10/part1.ts
+++ src/Day10/part1.ts
@@ -1,5 +1,69 @@
 import {readExample} from "../common/importer";
+import {Direction, Pipe, pipeTransformations} from "./pipes";
 
 const file = readExample();
+const pipes: Pipe[] = file.flatMap(
+    (line, index) => createPipes(line, index)
+)
 
-console.log(file);
+function createPipes(line: string, lineNumber: number): Pipe[] {
+    return line.trim().split('')
+        .map((symbol, index) => {
+            return {
+                position: {
+                    x: index,
+                    y: lineNumber,
+                },
+                transformations: pipeTransformations[symbol],
+                symbol: symbol
+            };
+        });
+
+}
+
+const startPipe = pipes.find(pipe => pipe.symbol === 'S');
+
+function nextPipe  (pipe: Pipe, direction: Direction, distance: number){
+    console.log('------------- distance: ' + distance);
+    const newDirection = pipe.transformations.find(transformation => transformation.input == direction).output;
+    const newPosition = move(pipe.position, newDirection);
+    const newPipe = pipes.find(pipe => pipe.position.x === newPosition.x && pipe.position.y === newPosition.y);
+    console.log(pipe.symbol +  ' -> ' + newPipe.symbol);
+    distance++;
+    if (newPipe.symbol === 'S'){
+        return distance;
+    }
+
+    return nextPipe(newPipe, newDirection, distance);
+}
+
+function move(position, direction){
+    if (direction == Direction.right){
+        return {
+            x: position.x + 1,
+            y: position.y
+        }
+    }
+    if (direction == Direction.left){
+        return {
+            x: position.x - 1,
+            y: position.y
+        }
+    }
+    if (direction == Direction.up){
+        return {
+            x: position.x,
+            y: position.y - 1
+        }
+    }
+    if (direction == Direction.down){
+        return {
+            x: position.x,
+            y: position.y + 1
+        }
+    }
+}
+
+
+const totaldistance = nextPipe(startPipe, Direction.right, 0);
+console.log(totaldistance / 2)
--- src/Day10/pipes.ts
+++ src/Day10/pipes.ts
@@ -1,3 +1,52 @@
-type pipe = {
-    
+export type Pipe = {
+    position: Position,
+    transformations: Transformation[],
+    symbol: string
+}
+
+export type Position = {
+    x: number,
+    y: number
+}
+
+export type Transformation = {
+    input: Direction,
+    output: Direction
+}
+
+export enum Direction {
+    up,
+    down,
+    left,
+    right
+}
+
+export const pipeTransformations: Record<string, Transformation[]> = {
+    '7': [
+        {input: Direction.up, output: Direction.left},
+        {input: Direction.right, output: Direction.down},
+    ],
+    'J': [
+        {input: Direction.down, output: Direction.left},
+        {input: Direction.right, output: Direction.up},
+    ],
+    'L': [
+        {input: Direction.down, output: Direction.right},
+        {input: Direction.left, output: Direction.up},
+    ],
+    'F': [
+        {input: Direction.up, output: Direction.right},
+        {input: Direction.left, output: Direction.down},
+    ],
+    '|': [
+        {input: Direction.up, output: Direction.up},
+        {input: Direction.down, output: Direction.down},
+    ],
+    '-': [
+        {input: Direction.right, output: Direction.right},
+        {input: Direction.left, output: Direction.left},
+    ],
+    'S': [
+        {input: Direction.right, output: Direction.right},
+    ]
 }
