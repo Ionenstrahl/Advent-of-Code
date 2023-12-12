2f2dabe4123b1975cfaf7f3d660d79761db15b02
jonas.hoerl <jonas_hoerl@web.de>
2023-12-12 12:22:03 +0100

    Day12.2: first copy of https://gitlab.com/RedPixel/aoc2023/-/blob/main/day12/day12.py

--- src/Day12/part2.ts
+++ src/Day12/part2.ts
@@ -5,10 +5,11 @@ function result() {
     return readExample()
         .map(line => line.trim().split(' '))
         .map(([plan, listing]) => ({plan, listing: listing.split(',').map(Number)}))
-        .map(({plan, listing}) => ({plan: unfoldPlan(plan), listing: unfoldListing(listing)}))
-        .map(({plan, listing}) => ({variations: generateOnlyValidVariations(plan, 0, [], listing), listing}))
-        .map(validVariationLists => validVariationLists.length)
-        .reduce((a, b) => a + b, 0);
+       // .map(({plan, listing}) => ({plan: unfoldPlan(plan), listing: unfoldListing(listing)}))
+        .map(({plan, listing}) => count(plan, listing, 0));
+    // .map(({plan, listing}) => ({variations: generateOnlyValidVariations(plan, 0, [], listing), listing}))
+    // .map(validVariationLists => validVariationLists.length)
+    // .reduce((a, b) => a + b, 0);
 }
 
 function unfoldPlan(plan: string): string {
@@ -35,6 +36,26 @@ export function generateOnlyValidVariations(input, index, current, correctListin
     }
 
     return generateOnlyValidVariations(input, index + 1, current + input[index], correctListing);
+}
+
+function count(plan: string, listing: number[], buffer: number): number {
+    if (!plan) {
+        return !listing && !buffer ? 1 : 0;
+    }
+
+
+    let result = 0;
+
+    if (['#', '?'].includes(plan[0])) {
+        result += count(plan.slice(1), listing, buffer + 1)
+    }
+
+    if ((['.', '?'].includes(plan[0])) && (listing && listing[0] === buffer || !buffer) ) {
+        const newListing: number[] = buffer > 0 ? listing.slice(1) : listing;
+        result += count(plan.slice(1), newListing, buffer + 1)
+    }
+
+    return result;
 }
 
 console.log(result())
