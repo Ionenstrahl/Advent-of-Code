22ea59266c0ecbf7b51cfe726b7a1113143706ea
jonas.hoerl <jonas_hoerl@web.de>
2023-12-12 13:00:10 +0100

    Day12.2: working example

--- src/Day12/part2.ts
+++ src/Day12/part2.ts
@@ -1,15 +1,12 @@
 import {readExample} from "../common/importer";
-import {validVariations, variationList} from "./part1";
 
 function result() {
     return readExample()
         .map(line => line.trim().split(' '))
         .map(([plan, listing]) => ({plan, listing: listing.split(',').map(Number)}))
-       // .map(({plan, listing}) => ({plan: unfoldPlan(plan), listing: unfoldListing(listing)}))
-        .map(({plan, listing}) => count(plan, listing, 0));
-    // .map(({plan, listing}) => ({variations: generateOnlyValidVariations(plan, 0, [], listing), listing}))
-    // .map(validVariationLists => validVariationLists.length)
-    // .reduce((a, b) => a + b, 0);
+        .map(({plan, listing}) => ({plan: unfoldPlan(plan), listing: unfoldListing(listing)}))
+        .map(({plan, listing}) => count(plan, listing))
+        .reduce((a, b) => a + b, 0);
 }
 
 function unfoldPlan(plan: string): string {
@@ -20,29 +17,13 @@ function unfoldListing(listing: number[]): number[] {
     return [].concat(listing).concat(listing).concat(listing).concat(listing).concat(listing)
 }
 
-export function generateOnlyValidVariations(input, index, current, correctListing) {
-    if (index === input.length) {
-        const currentList = variationList([current]);
-        if (validVariations(currentList, correctListing).length > 0) {
-            return [current];
+function count(plan: string, listing: number[], buffer: number = 0): number {
+    if (plan.length == 0) {
+        if (listing.length == 0 && buffer == 0 || listing.length == 1 && listing[0] == buffer) {
+            return 1;
         }
-        return [];
+        return 0;
     }
-
-    if (input[index] === '?') {
-        const hashVariation = generateOnlyValidVariations(input, index + 1, current + '#', correctListing);
-        const dotVariation = generateOnlyValidVariations(input, index + 1, current + '.', correctListing);
-        return hashVariation.concat(dotVariation);
-    }
-
-    return generateOnlyValidVariations(input, index + 1, current + input[index], correctListing);
-}
-
-function count(plan: string, listing: number[], buffer: number): number {
-    if (!plan) {
-        return !listing && !buffer ? 1 : 0;
-    }
-
 
     let result = 0;
 
@@ -50,9 +31,9 @@ function count(plan: string, listing: number[], buffer: number): number {
         result += count(plan.slice(1), listing, buffer + 1)
     }
 
-    if ((['.', '?'].includes(plan[0])) && (listing && listing[0] === buffer || !buffer) ) {
+    if ((['.', '?'].includes(plan[0])) && (listing.length > 0 && listing[0] == buffer || buffer == 0)) {
         const newListing: number[] = buffer > 0 ? listing.slice(1) : listing;
-        result += count(plan.slice(1), newListing, buffer + 1)
+        result += count(plan.slice(1), newListing)
     }
 
     return result;
