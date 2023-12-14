29b810aeb26800b282a99188f7e4ffdf9f6db388
jonas.hoerl <jonas_hoerl@web.de>
2023-12-14 11:14:05 +0100

    Day14.1: refactor

--- src/Day14/part1.ts
+++ src/Day14/part1.ts
@@ -1,9 +1,6 @@
-import {readExample} from "../common/importer";
+import { readExample } from "../common/importer";
 
-const file = readExample();
-
-
-function parseColumns(file: string[]) {
+function parseColumns(file: string[]): string[] {
     return file[0].trim().split('')
         .map((_, index: number) =>
             file.map((row: string) => row[index])
@@ -12,31 +9,33 @@ function parseColumns(file: string[]) {
 }
 
 function tiltNorth(column: string): string {
-    const tiltedColumn = column.replace('.O', 'O.');
+    const tiltedColumn: string = column.replace('.O', 'O.');
 
     if (tiltedColumn.includes('.O')) {
-        return tiltNorth(tiltedColumn)
+        return tiltNorth(tiltedColumn);
     }
 
     return tiltedColumn;
 }
 
-function torque(column: string){
-    const leverLength = column.length;
+function torque(column: string): number {
+    const leverLength: number = column.length;
 
     return column.split('')
-        .map((rock, index) => ({rock, index}))
-        .filter(({rock}) => rock === 'O')
-        .map(({index}) => leverLength - index)
-        .reduce((a, b) => a + b, 0);
+        .map((rock: string, index: number) => ({ rock, index }))
+        .filter(({ rock }: { rock: string }) => rock === 'O')
+        .map(({ index }: { index: number }) => leverLength - index)
+        .reduce((a: number, b: number) => a + b, 0);
 }
 
 
-const columns =
-    parseColumns(readExample())
-        .map(column => tiltNorth(column))
-        .map(column => torque(column))
-        .reduce((a, b) => a + b, 0);
+function totalTorque(): number {
+    const file: string[] = readExample();
 
+    return parseColumns(file)
+        .map((column: string) => tiltNorth(column))
+        .map((column: string) => torque(column))
+        .reduce((a: number, b: number) => a + b, 0)
+}
 
-console.log(columns);
+console.log(totalTorque());
